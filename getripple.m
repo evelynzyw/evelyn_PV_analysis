8256ab9dfef1f162057acf8d855ac9e140662f83
evelynzyw <133751316+evelynzyw@users.noreply.github.com>
2024-10-22 15:30:54 +0800

    Add files via upload

--- getripple.m
+++ getripple.m
@@ -1,19 +1,16 @@
 clear all;
 close all;
-% [n,p]=uigetfile;
-% filename=[p n]; %键入计算通道
-% channelinput=input('输入需要计算的通道，从1开始,记得多个通道需要加中括号，并用空格间开：'); %导入数据
-fpath = char("D:\JeffreyTong\data\LE14\LE14_231127\LE14_231127_lfp\LE14_231127.lfp");%导入数据
+load '.mat';%load data
+filparts=char'.mat'
 [filepath,subDirName,ext] = fileparts(fpath)
-channelsSelected = [1 2 3 4]; %选择有spike的通道数
+channelsSelected = [1 2 3 4]; 
 data_raw=readmulti_frank(fpath,32,channelsSelected,0,inf);
-data_raw1=data_raw*0.194; %导入时间
+data_raw1=data_raw*0.194; 
 data_raw=mean(data_raw1,2);
 time=((1:size(data_raw,1)))';
 lfp=[time/1250, data_raw];
 filtered = FilterLFP(lfp);
-% ripples1=[];
-% for i=2:size(filtered,2)
+
 i=2
 [ripples,sd,bad] = FindRipples(filtered(:,[1,i]));
 [maps,data,stats] = RippleStats(filtered(:,[1,i]),ripples);
